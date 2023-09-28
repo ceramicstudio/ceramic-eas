b5094436676f9d5df86c4a2c61d98dae67554a5c
Mark Krasner <markkrasner@marks-macbook.tail94015.ts.net>
2023-09-28 14:48:00 -0600

    changing to static hirenodes endpoint

--- scripts/composites.mjs
+++ scripts/composites.mjs
@@ -12,7 +12,7 @@ import { Ed25519Provider } from "key-did-provider-ed25519";
 import { getResolver } from "key-did-resolver";
 import { fromString } from "uint8arrays/from-string";
 
-const ceramic = new CeramicClient('http://localhost:7007');
+const ceramic = new CeramicClient("https://ceramic-temp.hirenodes.io");
 
 /**
  * @param {Ora} spinner - to provide progress status.
@@ -20,83 +20,83 @@ const ceramic = new CeramicClient('http://localhost:7007');
  */
 export const writeComposite = async (spinner) => {
   await authenticate();
-  spinner.info("writing composite to Ceramic");
+  // spinner.info("writing composite to Ceramic");
 
-  const researchComposite = await createComposite(
-    ceramic,
-    "./composites/00-researchObject.graphql"
-  );
+  // const researchComposite = await createComposite(
+  //   ceramic,
+  //   "./composites/00-researchObject.graphql"
+  // );
 
-  const attestationSchema = readFileSync("./composites/00-attestation.graphql", {
-    encoding: "utf-8",
-  }).replace("$RESEARCH_ID", researchComposite.modelIDs[0]);
+  // const attestationSchema = readFileSync("./composites/00-attestation.graphql", {
+  //   encoding: "utf-8",
+  // }).replace("$RESEARCH_ID", researchComposite.modelIDs[0]);
 
-  const attestationComposite = await Composite.create({
-    ceramic,
-    schema: attestationSchema,
-  });
+  // const attestationComposite = await Composite.create({
+  //   ceramic,
+  //   schema: attestationSchema,
+  // });
 
-  const confirmSchema = readFileSync("./composites/01-confirm.graphql", {
-    encoding: "utf-8",
-  }).replace("$ATTESTATION_ID", attestationComposite.modelIDs[1]);
+  // const confirmSchema = readFileSync("./composites/01-confirm.graphql", {
+  //   encoding: "utf-8",
+  // }).replace("$ATTESTATION_ID", attestationComposite.modelIDs[1]);
 
-  const confirmComposite = await Composite.create({
-    ceramic,
-    schema: confirmSchema,
-  });
+  // const confirmComposite = await Composite.create({
+  //   ceramic,
+  //   schema: confirmSchema,
+  // });
 
-  const confirmConnectSchema = readFileSync(
-    "./composites/02-confirmConnect.graphql",
-    {
-      encoding: "utf-8",
-    }
-  )
-    .replace("$CONFIRM_ID", confirmComposite.modelIDs[1])
-    .replace("$ATTESTATION_ID", attestationComposite.modelIDs[1]);
+  // const confirmConnectSchema = readFileSync(
+  //   "./composites/02-confirmConnect.graphql",
+  //   {
+  //     encoding: "utf-8",
+  //   }
+  // )
+  //   .replace("$CONFIRM_ID", confirmComposite.modelIDs[1])
+  //   .replace("$ATTESTATION_ID", attestationComposite.modelIDs[1]);
 
-  const confirmConnectComposite = await Composite.create({
-    ceramic,
-    schema: confirmConnectSchema,
-  });
+  // const confirmConnectComposite = await Composite.create({
+  //   ceramic,
+  //   schema: confirmConnectSchema,
+  // });
 
-  const gitcoinComposite = await createComposite(
-    ceramic,
-    "./composites/04-gitcoinPassport.graphql"
-  );
+  // const gitcoinComposite = await createComposite(
+  //   ceramic,
+  //   "./composites/04-gitcoinPassport.graphql"
+  // );
 
-  const vcPassportSchema = readFileSync("./composites/04-verifiableCredentialForGitcoinPassport.graphql", {
-    encoding: "utf-8",
-  }).replace("$PASSPORT_ID", gitcoinComposite.modelIDs[0]);
+  // const vcPassportSchema = readFileSync("./composites/04-verifiableCredentialForGitcoinPassport.graphql", {
+  //   encoding: "utf-8",
+  // }).replace("$PASSPORT_ID", gitcoinComposite.modelIDs[0]);
 
-  const vcPassportComposite = await Composite.create({
-    ceramic,
-    schema: vcPassportSchema,
-  });
+  // const vcPassportComposite = await Composite.create({
+  //   ceramic,
+  //   schema: vcPassportSchema,
+  // });
 
-  const composite = Composite.from([
-    researchComposite,
-    attestationComposite,
-    confirmComposite,
-    confirmConnectComposite,
-    gitcoinComposite,
-    vcPassportComposite
-  ]);
+  // const composite = Composite.from([
+  //   researchComposite,
+  //   attestationComposite,
+  //   confirmComposite,
+  //   confirmConnectComposite,
+  //   gitcoinComposite,
+  //   vcPassportComposite
+  // ]);
 
-  await writeEncodedComposite(composite, "./src/__generated__/definition.json");
-  spinner.info("creating composite for runtime usage");
-  await writeEncodedCompositeRuntime(
-    ceramic,
-    "./src/__generated__/definition.json",
-    "./src/__generated__/definition.js"
-  );
-  spinner.info("deploying composite");
-  const deployComposite = await readEncodedComposite(
-    ceramic,
-    "./src/__generated__/definition.json"
-  );
+  // await writeEncodedComposite(composite, "./src/__generated__/definition.json");
+  // spinner.info("creating composite for runtime usage");
+  // await writeEncodedCompositeRuntime(
+  //   ceramic,
+  //   "./src/__generated__/definition.json",
+  //   "./src/__generated__/definition.js"
+  // );
+  // spinner.info("deploying composite");
+  // const deployComposite = await readEncodedComposite(
+  //   ceramic,
+  //   "./src/__generated__/definition.json"
+  // );
 
-  await deployComposite.startIndexingOn(ceramic);
-  spinner.succeed("composite deployed & ready for use");
+  // await deployComposite.startIndexingOn(ceramic);
+  spinner.succeed("node authenticated");
 };
 
 /**
